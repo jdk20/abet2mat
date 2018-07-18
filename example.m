ed6b21679b4182ac314f1d77a6188898dcf4fbc9
Jon Kenny <jkenny@caltech.edu>
2018-07-18 11:44:35 -0700

    Bug commments and cleaned up spacing

--- example.m
+++ example.m
@@ -1,4 +1,6 @@
 %% abet2mat example
+% Jon Kenny (jkenny@caltech.edu)
+% Be on the lookout for any bugs
 clc; clear variables; close all;
 
 % Call function to import trial, ITI, reward collection, and IR data
